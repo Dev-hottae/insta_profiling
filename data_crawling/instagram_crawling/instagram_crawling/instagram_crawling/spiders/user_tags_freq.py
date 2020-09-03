dd818106d45c41cf37f0372a0678af9845ef5a55
Dev-hottae <hottae0805@gmail.com>
2020-09-03 13:28:15 +0900

    user_tags_crawler finished

--- data_crawling/instagram_crawling/instagram_crawling/instagram_crawling/spiders/user_tags_freq.py
+++ data_crawling/instagram_crawling/instagram_crawling/instagram_crawling/spiders/user_tags_freq.py
@@ -15,13 +15,23 @@ class UserTagsSpider(scrapy.Spider):
 
     url_format = "https://www.instagram.com/graphql/query/?query_hash=bfa387b2992c3a52dcbe447467b4b771&variables="
 
-    def __init__(self, path, hold_time=601, min_post_count=0, max_post_count=1000, **kwargs):
+    def __init__(self, path, from_date=20200101, hold_time=601, min_post_count=15, max_post_count=500, **kwargs):
+        '''
+        :param path: innerid 가 들어있는 json 데이터 경로
+        :param from_date: 언제이후의 포스트 가져올지 설정
+        :param hold_time: 429 에러 시 hold 하고 있을 time(초)
+        :param min_post_count: 크롤링할 유저의 최소 포스트 갯수(2020.01.01 이후로)
+        :param max_post_count: 크롤링할 유저의 최대 포스트 갯수(2020.01.01 이후로)
+        :param kwargs:
+        '''
+
         super().__init__(**kwargs)
 
         data = pd.read_json(path, encoding='utf-8')
         self.user_id_list = data['innerid'].tolist()
         self.min_post_count = min_post_count
         self.max_post_count = max_post_count
+        self.from_date = datetime.timestamp(datetime.strptime(str(from_date), "%Y%m%d"))
 
         UserTagsSpider.HOLD_TIME = hold_time
 
@@ -41,14 +51,14 @@ class UserTagsSpider(scrapy.Spider):
 
         tags_dict = defaultdict(int)
 
-        # 포스트 1000개 미만 유저만
-        if (post_count < self.max_post_count):
+        # 포스트 max 미만 유저만 & min 이상 유저만
+        if (post_count < self.max_post_count) and (post_count > self.min_post_count):
 
             for post in posts:
                 post_time = post['node']['taken_at_timestamp']
 
-                # 2020년 1월 1일 00시
-                if post_time > 1577836800:
+                # 2020년 1월 1일 00시(default)
+                if post_time > self.from_date:
                     tags = self.tag_extraction(post['node']['edge_media_to_caption']['edges'][0]['node']['text'])
 
                     for tag in tags:
